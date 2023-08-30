4026381e8bcd09a358e7b9f81e70c5b329abc7e0
Darsh Mashru <mashrudarsh31@gmail.com>
2023-08-30 17:38:52 +0530

    Update 2

--- app/src/main/java/com/darshmashru/madassignment1/MoonPhotosPage.java
+++ app/src/main/java/com/darshmashru/madassignment1/MoonPhotosPage.java
@@ -2,7 +2,9 @@ package com.darshmashru.madassignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MoonPhotosPage extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MoonPhotosPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moon_photos_page);
+    }
+    public void goBack(View view){
+        Intent goback = new Intent(MoonPhotosPage.this, MainActivity.class);
+        startActivity(goback);
     }
 }
