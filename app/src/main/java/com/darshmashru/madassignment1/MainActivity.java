61eb6886972485426c1c46f3f7b0dfc7c92779cc
Darsh Mashru <mashrudarsh31@gmail.com>
2023-08-30 17:26:11 +0530

    Update 1

---
+++ app/src/main/java/com/darshmashru/madassignment1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.darshmashru.madassignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void nextPage(View view){
+        Intent nextPage = new Intent(MainActivity.this, MoonPhotosPage.class);
+        startActivity(nextPage);
+    }
+}
---
+++ app/src/main/java/com/darshmashru/madassignment1/MoonPhotosPage.java
@@ -0,0 +1,14 @@
+package com.darshmashru.madassignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoonPhotosPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moon_photos_page);
+    }
+}
